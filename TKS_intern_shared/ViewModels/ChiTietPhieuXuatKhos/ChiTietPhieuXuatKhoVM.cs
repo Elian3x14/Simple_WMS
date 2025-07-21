3e14b1b6560a1a3deefdc164243080933f1656e4
limbanga <thliem143@gmail.com>
2025-07-21 11:02:42 +0700

    Enhance view models in Edit.razor and ChiTietPhieuXuatKhoVM
    Updated Edit.razor to include the SanPham property in
    ChiTietPhieuXuatKhoSaveVM, allowing for better detail
    processing. Changed PhieuXuatKho and SanPham properties
    in ChiTietPhieuXuatKhoVM to use their respective view
    models, improving separation of concerns for UI data binding.

--- TKS_intern_shared/ViewModels/ChiTietPhieuXuatKhos/ChiTietPhieuXuatKhoVM.cs
+++ TKS_intern_shared/ViewModels/ChiTietPhieuXuatKhos/ChiTietPhieuXuatKhoVM.cs
@@ -4,16 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TKS_intern_shared.Models;
+using TKS_intern_shared.ViewModels.PhieuXuatKhos;
+using TKS_intern_shared.ViewModels.SanPhams;
 
 namespace TKS_intern_shared.ViewModels.ChiTietPhieuXuatKhos
 {
     public class ChiTietPhieuXuatKhoVM : BaseVM
     {
         public required int PhieuXuatKhoId { get; set; }
-        public PhieuXuatKho? PhieuXuatKho { get; set; }
+        public PhieuXuatKhoVM? PhieuXuatKho { get; set; }
 
         public required int SanPhamId { get; set; }
-        public required SanPham SanPham { get; set; }
+        public required SanPhamVM SanPham { get; set; }
         public required int SoLuongXuat { get; set; }
         public required decimal DonGiaXuat { get; set; }
     }
